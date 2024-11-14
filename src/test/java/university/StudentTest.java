db3b2d1e93a83e9e314e4b422b267ae22d367854
yellowcanstop <yellowcanstop@gmail.com>
2024-11-14 12:12:51 +0800

    Break test by removing e in hello

--- src/test/java/university/StudentTest.java
+++ src/test/java/university/StudentTest.java
@@ -11,6 +11,6 @@ public class StudentTest {
 
   @Test
   public void studentSaysHello() {
-    assertThat(student.sayHello(), containsString("Hello"));
+    assertThat(student.sayHello(), containsString("Hllo"));
   }
 }
